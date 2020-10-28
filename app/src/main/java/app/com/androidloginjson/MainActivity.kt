cc942b19d62c1ccf65cb605a471a174c423c3a91
Miguel Angel <michirapper2001@gmail.com>
2020-10-28 18:16:47 +0100

    insertar usuarios

--- app/src/main/java/app/com/androidloginjson/MainActivity.kt
+++ app/src/main/java/app/com/androidloginjson/MainActivity.kt
@@ -1,11 +1,66 @@
 package app.com.androidloginjson
 
-import androidx.appcompat.app.AppCompatActivity
+import android.content.Context
+import android.content.Intent
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.TextView
+import androidx.appcompat.app.AppCompatActivity
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.File
+import java.io.FileOutputStream
+import java.io.InputStreamReader
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var btnRegistro = findViewById<Button>(R.id.btnRegistro)
+        var btnLogin = findViewById<Button>(R.id.btnLogin)
+        var btnInformacion = findViewById<Button>(R.id.btnInformacion)
+        btnInformacion.setEnabled(false)
+        btnLogin.setEnabled(false)
+
+        var nombreFichero = "ficheroInterno.json"
+
+        var textoNoExiste = "{\"usuarios\": []}"
+
+        //Comprobar si el JSON existe
+
+        var file = File(getFilesDir().getAbsolutePath(), nombreFichero)
+        if (!file.exists()) {
+            var fileOutput = openFileOutput(nombreFichero, Context.MODE_PRIVATE)
+            fileOutput.write(textoNoExiste.toByteArray())
+            fileOutput.close()
+        }
+
+        //Leer todo el texto
+
+        var bufferedReader = BufferedReader(InputStreamReader(openFileInput(nombreFichero)))
+        var textoLeido = bufferedReader.readLine()
+        val todo = StringBuilder()
+        while (textoLeido != null) {
+            todo.append(textoLeido + "\n")
+            textoLeido = bufferedReader.readLine()
+        }
+        bufferedReader.close()
+        textoLeido = todo.toString()
+
+        //Comprobar si hay algo en el fichero y deshabilitar botones
+        
+        val jsonObject = JSONObject(textoLeido)
+        val jsonArray = jsonObject.optJSONArray("usuarios")
+        if (!jsonArray.toString().equals("[]")) {
+            btnLogin.setEnabled(true)
+        }
+
+    }
+
+    fun goRegistro(view: View) {
+        var miIntent = Intent(this, Registro::class.java)
+        startActivity(miIntent)
     }
 }
---
+++ app/src/main/java/app/com/androidloginjson/Registro.kt
@@ -0,0 +1,54 @@
+package app.com.androidloginjson
+
+import android.content.Context
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import org.json.JSONObject
+import java.io.BufferedReader
+import java.io.InputStreamReader
+
+class Registro : AppCompatActivity() {
+    var num = 0
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_registro)
+    }
+
+    fun Cancelar(view: View) {
+        onBackPressed()
+    }
+
+    fun Anadir(view: View) {
+        var nombre = findViewById<EditText>(R.id.editTextTextNombre)
+        var apellidos = findViewById<EditText>(R.id.editTextTextApellidos)
+        var user = findViewById<EditText>(R.id.editTextTextUsuario)
+        var pass = findViewById<EditText>(R.id.editTextTextContrasena)
+        var nombreFichero = "ficheroInterno.json"
+        var bufferedReader = BufferedReader(InputStreamReader(openFileInput(nombreFichero)))
+        var textoLeido = bufferedReader.readLine()
+        val todo = StringBuilder()
+        while (textoLeido != null) {
+            todo.append(textoLeido + "\n")
+            textoLeido = bufferedReader.readLine()
+        }
+        textoLeido = todo.toString()
+        val jsonObject = JSONObject(textoLeido)
+        val jsonArray = jsonObject.optJSONArray("usuarios")
+        for (i in 0 until jsonArray.length()) {
+            num = i
+        }
+        val main = JSONObject()
+        main.put("id", num+1)
+        main.put("nombre", nombre.text)
+        main.put("Apellido", apellidos.text)
+        main.put("user", user.text)
+        main.put("contrasena", pass.text)
+        jsonArray.put(main)
+        var fileOutput = openFileOutput(nombreFichero, Context.MODE_PRIVATE)
+        fileOutput.write(jsonObject.toString().toByteArray())
+        fileOutput.close()
+    }
+}
